4335a86600a67c43cf548a531359ad64a01a307a
Emily N Mayeski <enmayeski@gmail.com>
2021-10-11 12:50:33 -0400

    added typeDefs - subject to change

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,38 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
-  }
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: ID
+  authors: [Author]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(input: bookInfo): User
+  RemoveBook(bookId: ID!): User
+}
 `;
 // export the typeDefs
 module.exports = typeDefs;
